9b860bde99d7b599c88045547b4975f6e20cc2d7
Abdul Samad <asamad384@gmail.com>
2020-05-24 04:17:37 +0500

    Updated

--- js/includes/covidglobal.js
+++ js/includes/covidglobal.js
@@ -1,5 +1,5 @@
 /*  
-//Project Name: Live With Covid-19
+Project Name: Live With Covid-19
 Project URI: 
 Description: The Covid-19 App designed and developed for practicing purpose. It is an open-source project. You can freely use it for educational purposes. If you are intending to use it with your commercial projects then you will need permission.
 Author: Abdul Samad
